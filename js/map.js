6db6685ba0c3f169e74bd687617cc05c053e204c
garmatueur <remi.vandewalle@isen.yncrea.fr>
2019-01-14 16:26:12 +0100

    zoom

--- js/map.js
+++ js/map.js
@@ -10,7 +10,7 @@ var initMap = function() {
   L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
     // Il est toujours bien de laisser le lien vers la source des données
     attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-    minZoom: 1,
+    minZoom: 14,
     maxZoom: 20
   }).addTo(macarte);
   Markers();
