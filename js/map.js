6e53bedde79ef5f66a089b1f3783dbbe260d8638
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-01-14 13:51:51 +0100

    js and css for map

---
+++ js/map.js
@@ -0,0 +1,18 @@
+// On initialise la latitude et la longitude de Paris (centre de la carte)
+var lat = 50.6326571;
+var lon = 3.0360376;
+var macarte = null;
+// Fonction d'initialisation de la carte
+var initMap = function() {
+  // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+  macarte = L.map('map').setView([lat, lon], 15);
+  // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+    // Il est toujours bien de laisser le lien vers la source des données
+    attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+    minZoom: 1,
+    maxZoom: 20
+  }).addTo(macarte);
+  var marker = L.marker([50.63394, 3.04875]).addTo(macarte);
+  var marker = L.marker([50.6331, 3.02359]).addTo(macarte);
+};
