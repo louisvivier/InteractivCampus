d6358856416cc0c3b6cd563ce082bf23d6682c1c
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-01-15 16:21:55 +0100

    pan files (in dev)

--- js/pan.js
+++ js/pan.js
@@ -2,6 +2,12 @@ var leftPan = {
   open(id){
     document.getElementById("leftPan").style.visibility = "visible";
     document.getElementById("leftPan").style.width = "500px";
+    document.getElementsByClassName("logo")[0].setAttribute("src","images/logos/"+id+".png");
+    document.getElementsByClassName("streetpic")[0].setAttribute("src","images/streetpics/"+id+".png");
+    document.getElementsByClassName("adress")[0].innerHTML = buildings[id].adress_street + ", " + buildings[id].street_city;
+    document.getElementsByClassName("phone")[0].innerHTML = buildings[id].phone;
+    document.getElementsByClassName("email")[0].innerHTML = buildings[id].email;
+    document.getElementsByClassName("website")[0].innerHTML = '<a href="'+buildings[id].website+'">'+buildings[id].website+'</a>' ;
   },
   close(){
     document.getElementById("leftPan").style.visibility = "hidden";
