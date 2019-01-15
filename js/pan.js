57738bc17374e2e06e0fd2b7b0d2d18fbb92d2df
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-01-15 20:57:43 +0100

    add info in pan

--- js/pan.js
+++ js/pan.js
@@ -1,15 +1,19 @@
 var leftPan = {
   open(id){
-    document.getElementById("leftPan").style.visibility = "visible";
-    document.getElementById("leftPan").style.width = "500px";
-    document.getElementsByClassName("logo")[0].setAttribute("src","images/logos/"+id+".png");
-    document.getElementsByClassName("streetpic")[0].setAttribute("src","images/streetpics/"+id+".png");
+    $("#leftPan").css("background-color",places[id].marker_color);
+    $("#leftPan").css("visibility","visible");
+    $("#leftPan").css("width","500px");
+    $(".logo").attr("src", "images/logos/"+id+".png");
+    $(".streetpic").attr("src", "images/streetpics/"+id+".png");
     $('.adress').html(places[id].adress_street + ", " + places[id].adress_city);
     $('.phone').html(places[id].phone);
     $('.email').html(places[id].email);
-    document.getElementsByClassName("website")[0].innerHTML = '<a target="_blank" href="http://'+places[id].website+'">'+places[id].website+'</a>' ;
+    $('.website').html('<a target="_blank" href="http://'+places[id].website+'">'+places[id].website+'</a>') ;
     if (places[id].twitter != ""){
       $('.twitterFeed').html('<a class="twitter-timeline" data-width="480" data-dnt="true" href="https://twitter.com/'+places[id].twitter+'?ref_src=twsrc%5Etfw" data-tweet-limit="5">Tweets by '+places[id].twitter+'</a> <script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>');
+    }
+    else{
+      $('.twitterFeed').html('');
     }
   },
   close(){
@@ -17,3 +21,8 @@ var leftPan = {
     document.getElementById("leftPan").style.width = "0";
   }
 }
+//blue #8eabc8
+//Red #d65543
+//Moutarde #dfc277
+//Vert #9eb886
+//Gris #adadae
