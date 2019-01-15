cd95e03f408066fb4a349b6aea28cde95d1a39e3
LCgenial <louis-clement.langue@isen.yncrea.fr>
2019-01-15 10:21:24 +0100

    Ajout Batiments.js

--- js/batiments.js
+++ js/batiments.js
@@ -1,3 +1,37 @@
-var Batiments = function (){
-
-};
+var Batiments = {
+  ISEN:{
+    "adresse_rue": "41 Boulevard Vauban",
+    "adresse_ville": "59800 Lille",
+    "telephone": "03 20 30 40 50",
+    "mail": "lille@isen.fr",
+    "site": "www.isen-lille.fr",
+  },
+  "HEI":{
+    "adresse_rue": "13 Rue de Toul",
+    "adresse_ville": "59000 Lille",
+    "telephone": "03 28 38 48 58",
+    "mail": "contact@hei.fr",
+    "site": "www.hei.fr"
+  },
+  "ISA":{
+    "adresse_rue": "48 Boulevard Vauban",
+    "adresse_ville": "59046 Lille",
+    "telephone": "03 28 38 48 48",
+    "mail": "contact@isa-lille.fr",
+    "site": "www.isa-lille.fr"
+  },
+  "yncrea":{
+    "adresse_rue": "2 Rue Norbert Segard",
+    "adresse_ville": "59014 Lille",
+    "telephone": "03 28 38 48 58",
+    "mail": "contact@yncrea.fr",
+    "site": "http://www.yncrea-hautsdefrance.fr"
+  },
+  "urbawood":{
+    "adresse_rue": "58 Allée Marie-Thérèse Vicot-Lhermitte",
+    "adresse_ville": "59000 Lille",
+    "telephone": "03 28 38 48 58",
+    "mail": "contac@yncrea.fr",
+    "site": "http://www.yncrea-hautsdefrance.fr"
+  }
+}
