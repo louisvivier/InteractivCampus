ce2bca1dcaa8eb01c158bf2d835d3314883258df
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-01-15 19:55:23 +0100

    add twitter feed on info pan

--- js/pan.js
+++ js/pan.js
@@ -4,10 +4,13 @@ var leftPan = {
     document.getElementById("leftPan").style.width = "500px";
     document.getElementsByClassName("logo")[0].setAttribute("src","images/logos/"+id+".png");
     document.getElementsByClassName("streetpic")[0].setAttribute("src","images/streetpics/"+id+".png");
-    document.getElementsByClassName("adress")[0].innerHTML = buildings[id].adress_street + ", " + buildings[id].street_city;
-    document.getElementsByClassName("phone")[0].innerHTML = buildings[id].phone;
-    document.getElementsByClassName("email")[0].innerHTML = buildings[id].email;
-    document.getElementsByClassName("website")[0].innerHTML = '<a href="'+buildings[id].website+'">'+buildings[id].website+'</a>' ;
+    $('.adress').html(places[id].adress_street + ", " + places[id].adress_city);
+    $('.phone').html(places[id].phone);
+    $('.email').html(places[id].email);
+    document.getElementsByClassName("website")[0].innerHTML = '<a target="_blank" href="http://'+places[id].website+'">'+places[id].website+'</a>' ;
+    if (places[id].twitter != ""){
+      $('.twitterFeed').html('<a class="twitter-timeline" data-width="480" data-dnt="true" href="https://twitter.com/'+places[id].twitter+'?ref_src=twsrc%5Etfw" data-tweet-limit="5">Tweets by '+places[id].twitter+'</a> <script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>');
+    }
   },
   close(){
     document.getElementById("leftPan").style.visibility = "hidden";
