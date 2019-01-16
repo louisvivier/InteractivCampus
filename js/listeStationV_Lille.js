8b63a6ed93de046c2f308c3219a85bd01e3481de
LCgenial <louis-clement.langue@isen.yncrea.fr>
2019-01-16 16:40:36 +0100

    Donnees V-Lille

--- js/listeStationV_Lille.js
+++ js/listeStationV_Lille.js
@@ -1,3 +1,9 @@
 var listeStationV_Lille = {
   "solferino":["PALAIS+RAMEAU", "LILLE"],
+  "euratechnologie":["EURATECHNOLOGIE", "LILLE"],
+  "boisblanc":["BOIS+BLANCS", "LILLE"],
+  "cormontaigne":["CORMONTAIGNE", "LILLE"],
+  "universitecatholique":["UNIVERSITE+CATHOLIQUE"],
+  "hei":["RUE+DE+TOUL", "LILLE"],
+  "republique":["REPUBLIQUE+BEAUX+ARTS", "LILLE"],
 };
