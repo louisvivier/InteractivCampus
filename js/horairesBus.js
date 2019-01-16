05ba490df7dca01942517a5c28084c704e3ba14d
LCgenial <louis-clement.langue@isen.yncrea.fr>
2019-01-16 14:52:12 +0100

    Update horairesBus.js

--- js/horairesBus.js
+++ js/horairesBus.js
@@ -1,86 +1,87 @@
-var soustractionHeure = function(heure1, minute1, heure2, minute2){
-  var heure, minute;
-  if(minute2-minute1 < 0){
-    minute2 = minute2 + 60;
-    heure2 = heure2 - 1;
-    minute = minute2-minute1;
-    if(heure2- heure1 < 0){
-      heure2 = heure2 + 24;
-      heure = heure2 - heure1;
+var horairesBus = {
+  soustractionHeure(heure1, minute1, heure2, minute2){
+    var heure, minute;
+    if(minute2-minute1 < 0){
+      minute2 = minute2 + 60;
+      heure2 = heure2 - 1;
+      minute = minute2-minute1;
+      if(heure2- heure1 < 0){
+        heure2 = heure2 + 24;
+        heure = heure2 - heure1;
+      }else{
+        heure = heure2 - heure1;
+      }
     }else{
-      heure = heure2 - heure1;
+      minute = minute2-minute1;
+      if(heure2- heure1 < 0){
+        heure2 = heure2 + 24;
+        heure = heure2 - heure1;
+      }else{
+        heure = heure2 - heure1;
+      }
     }
-  }else{
-    minute = minute2-minute1;
-    if(heure2- heure1 < 0){
-      heure2 = heure2 + 24;
-      heure = heure2 - heure1;
+    var resultat = [heure, minute];
+    return resultat;
+  },
+
+  stringSuivant(heure, minute){
+    var res;
+    if(heure == 0 && minute == 0){
+      res = ' un instant';
+    }else if(heure == 0){
+      if(minute == 1){
+        res = ' une minute';
+      }else{
+        res = " " + minute + " minutes";
+      }
     }else{
-      heure = heure2 - heure1;
+      res = " " + heure + "h" + minute;
     }
+    return res;
+  },
+
+  tempsRestant(suivant, second, troisieme){
+    var horaireActuel = new Date();
+    var heureActuel = horaireActuel.getUTCHours();
+    var minuteActuel = horaireActuel.getUTCMinutes();
+    var horaire1 = new Date(suivant);
+    var heure1 = horaire1.getUTCHours();
+    var minute1 = horaire1.getUTCMinutes();
+    var resultat1 = horairesBus.soustractionHeure(heureActuel, minuteActuel, heure1, minute1);
+    heure1 = resultat1[0];
+    minute1 = resultat1[1];
+    var horaire2 = new Date(second);
+    var heure2 = horaire2.getUTCHours();
+    var minute2 = horaire2.getUTCMinutes();
+    var resultat2 = horairesBus.soustractionHeure(heureActuel, minuteActuel, heure2, minute2);
+    heure2 = resultat2[0];
+    minute2 = resultat2[1];
+    var horaire3 = new Date(troisieme);
+    var heure3 = horaire3.getUTCHours();
+    var minute3 = horaire3.getUTCMinutes();
+    var resultat3 = horairesBus.soustractionHeure(heureActuel, minuteActuel, heure3, minute3);
+    heure3 = resultat3[0];
+    minute3 = resultat3[1];
+    var temps1 = horairesBus.stringSuivant(heure1, minute1);
+    var temps2 = horairesBus.stringSuivant(heure2, minute2);
+    var temps3 = horairesBus.stringSuivant(heure3, minute3);
+    var horaires = [temps1, temps2, temps3];
+    return horaires;
+  },
+
+  getHoraires(ligne, arret, sens){
+    var xhr = new XMLHttpRequest();
+    var chemin = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=transpole-prochainspassages&facet=nomstation&facet=codeligne&facet=sensligne&refine.codeligne=" + ligne + "&refine.nomstation=" + arret + "&refine.sensligne=" + sens;
+    xhr.open('GET', chemin, false);
+    xhr.send(null);
+    var requete = xhr.response;
+    var suivant1 = requete.records[0].fields.heureestimeedepart;
+    var suivant2 = requete.records[1].fields.heureestimeedepart;
+    var suivant3 = requete.records[2].fields.heureestimeedepart;
+    var horaires = horairesBus.tempsRestant(suivant1, suivant2, suivant3);
+    return horaires;
   }
-  var resultat = [heure, minute];
-  return resultat;
 };
-
-var stringSuivant = function(heure, minute){
-  var res;
-  if(heure == 0 && minute == 0){
-    res = ' un instant';
-  }else if(heure == 0){
-    if(minute == 1){
-      res = ' une minute';
-    }else{
-      res = " " + minute + " minutes";
-    }
-  }else{
-    res = " " + heure + "h" + minute;
-  }
-  return res;
-};
-
-var tempsRestant = function(suivant, second, troisieme){
-  var horaireActuel = new Date();
-  var heureActuel = horaireActuel.getUTCHours();
-  var minuteActuel = horaireActuel.getUTCMinutes();
-  var horaire1 = new Date(suivant);
-  var heure1 = horaire1.getUTCHours();
-  var minute1 = horaire1.getUTCMinutes();
-  var resultat1 = soustractionHeure(heureActuel, minuteActuel, heure1, minute1);
-  heure1 = resultat1[0];
-  minute1 = resultat1[1];
-  var horaire2 = new Date(second);
-  var heure2 = horaire2.getUTCHours();
-  var minute2 = horaire2.getUTCMinutes();
-  var resultat2 = soustractionHeure(heureActuel, minuteActuel, heure2, minute2);
-  heure2 = resultat2[0];
-  minute2 = resultat2[1];
-  var horaire3 = new Date(troisieme);
-  var heure3 = horaire3.getUTCHours();
-  var minute3 = horaire3.getUTCMinutes();
-  var resultat3 = soustractionHeure(heureActuel, minuteActuel, heure3, minute3);
-  heure3 = resultat3[0];
-  minute3 = resultat3[1];
-  var temps1 = stringSuivant(heure1, minute1);
-  var temps2 = stringSuivant(heure2, minute2);
-  var temps3 = stringSuivant(heure3, minute3);
-  var horaires = [temps1, temps2, temps3];
-  return horaires;
-};
-
-var getHoraires = function(ligne, arret, sens){
-  var xhr = new XMLHttpRequest();
-  var chemin = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=transpole-prochainspassages&facet=nomstation&facet=codeligne&facet=sensligne&refine.codeligne=" + ligne + "&refine.nomstation=" + arret + "&refine.sensligne=" + sens;
-  xhr.open('GET', chemin, false);
-  xhr.send(null);
-  var requete = xhr.response;
-  var suivant1 = requete.records[0].fields.heureestimeedepart;
-  var suivant2 = requete.records[1].fields.heureestimeedepart;
-  var suivant3 = requete.records[2].fields.heureestimeedepart;
-  var horaires = tempsRestant(suivant1, suivant2, suivant3);
-  return horaires;
-};
-
 /*
 {
   "nhits":3,
