d96f6ca32ae052912238096fee64bc0c56b8573a
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-01-16 15:54:50 +0100

    add Name and Next Bus

--- js/pan.js
+++ js/pan.js
@@ -5,9 +5,16 @@ var leftPan = {
     $("#leftPan").css("width","500px");
     $(".logo").attr("src", "images/logos/"+places[id].logo_name+".png");
     $(".streetpic").attr("src", "images/streetpics/"+places[id].streetpic_name+".png");
+    $('.name').html(places[id].name);
     $('.adress').html(places[id].adress_street + ", " + places[id].adress_city);
     $('.phone').html(places[id].phone);
     $('.email').html(places[id].email);
+    if (1==1){
+      $('.NextBus').html("Prochain bus dans : "+horairesBus.getHoraires("18","SOLFERINO","ANATOLE+FRANCE")[0]+"<br>"+"Puis dans : "+horairesBus.getHoraires("18","SOLFERINO","ANATOLE+FRANCE")[1]+"<br>"+"Ou encore dans : "+horairesBus.getHoraires("18","SOLFERINO","ANATOLE+FRANCE")[2]);
+    }
+    else{
+      $('.NextBus').css("display","none");
+    }
     $('.website').html('<a target="_blank" href="'+places[id].website+'">'+places[id].link_text+'</a>') ;
     if (places[id].twitter != ""){
       $('.twitterFeed').html('<a class="twitter-timeline" data-width="480" data-dnt="true" href="https://twitter.com/'+places[id].twitter+'?ref_src=twsrc%5Etfw" data-tweet-limit="5">Tweets by '+places[id].twitter+'</a> <script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>');
