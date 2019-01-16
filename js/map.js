12f9717cfae007eb0d9c9a9a5ad3b57f6a695b71
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-01-16 10:14:51 +0100

    updated map bounce

--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@ var map = {
     // On initialise la latitude et la longitude de Paris (centre de la carte)
     let lat = 50.6326571;
     let lon = 3.0360376;
-    let southWest = L.latLng(50.6232,3.0180),northEast = L.latLng(50.6410,3.0816),bounds = L.latLngBounds(southWest, northEast);
+    let southWest = L.latLng(50.6090,2.9573),northEast = L.latLng(50.6540,3.1213),bounds = L.latLngBounds(southWest, northEast);
     // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
     macarte = L.map('map', {maxBounds: bounds}).setView([lat, lon], 15);
     // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
